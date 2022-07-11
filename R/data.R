fa1528f0fe50f5dd94785b22db8e12044cbc63fa
Alexander Coppock <acoppock@gmail.com>
2022-07-11 14:28:25 -0400

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,7 @@
+
+#' Table 2.1 from FEDAI
+#'
+#' This hypothetical dataset shows the treated (`Y_Z_1`) and untreated (`Y_Z_0`) potential outcomes for all seven villages, as well as a column for their difference, the village-level treatment effect `tau_i`
+#'
+#' @format A data.frame
+"table_2.1"
