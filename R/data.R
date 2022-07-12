407a84179baf1d322783d3948f5ecd16c25fbaca
Alex Coppock <acoppock@gmail.com>
2022-07-12 15:00:52 -0400

    table 2.2

--- R/data.R
+++ R/data.R
@@ -5,3 +5,11 @@
 #'
 #' @format A data.frame
 "table_2.1"
+
+
+#' Table 2.2 from FEDAI
+#'
+#' This hypothetical dataset shows a realized assignment (`Z`) and the corresponding revealed outcome (`Y`). Unobserved treated (`Y_Z_1`) and untreated (`Y_Z_0`) potential outcomes are marked as `NA` as is the village-level treatment effect `tau_i`.
+#'
+#' @format A data.frame
+"table_2.2"
