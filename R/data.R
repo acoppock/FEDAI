c69b04b76a43c4f645d0d09962c49bf51b1660ed
Alexander Coppock <acoppock@users.noreply.github.com>
2025-03-12 13:38:41 -0400

    add remaining datasets; rename with underscores

--- R/data.R
+++ R/data.R
@@ -1,94 +1,3 @@
-
-#' Table 2.1 from FEDAI
-#'
-#' This hypothetical dataset shows the treated (`Y_Z_1`) and untreated (`Y_Z_0`) potential outcomes for all seven villages, as well as a column for their difference, the village-level treatment effect `tau_i`
-#'
-#' @format A data.frame
-"table_2.1"
-
-
-#' Table 2.2 from FEDAI
-#'
-#' This hypothetical dataset shows a realized assignment (`Z`) and the corresponding revealed outcome (`Y`). Unobserved treated (`Y_Z_1`) and untreated (`Y_Z_0`) potential outcomes are marked as `NA` as is the village-level treatment effect `tau_i`.
-#'
-#' @format A data.frame
-"table_2.2"
-
-
-#' Box 3.7 from FEDAI
-#'
-#' This hypothetical dataset has two columns, a realized treatment (`Z`) and a revealed outcome (`Y`). The purpose of the dataset is to illustrate how traditional t-tests and randomization inference can yield different answers
-#'
-#' @format A data.frame
-"box_3.7"
-
-
-#' Table 3.3 from FEDAI
-#'
-#' This hypothetical dataset has the treated (`Y_Z_1`) and untreated (`Y_Z_0`) potential outcomes for 14 villages in two blocks.
-#'
-#' @format A data.frame
-"table_3.3"
-
-
-
-#' Table 3.6 from FEDAI
-#'
-#' This hypothetical dataset has the treated (`Y_Z_1`) and untreated (`Y_Z_0`) potential outcomes for 12 classrooms in 4 schools (high sampling variability)
-#'
-#' @format A data.frame
-"table_3.6"
-
-
-#' Table 3.7 from FEDAI
-#'
-#' This hypothetical dataset has the treated (`Y_Z_1`) and untreated (`Y_Z_0`) potential outcomes for 12 classrooms in 4 schools (low sampling variability)
-#'
-#' @format A data.frame
-"table_3.7"
-
-#' Table 4.1 from FEDAI
-#'
-#' Schedule of potential outcomes, pretest scores, and group assignments for simulated teacher incentives experiment
-#'
-#' @format A data.frame
-"table_4.1"
-
-#' Table 4.2 from FEDAI
-#'
-#' Block random assignment of teacher incentives experiment
-#'
-#' @format A data.frame
-"table_4.2"
-
-#' Table 5.1 from FEDAI
-#'
-#' Hypothetical schedule of potential outcomes assuming one-sided noncompliance
-#'
-#' @format A data.frame
-"table_5.1"
-
-#' Table 6.4 from FEDAI
-#'
-#' Hypothetical schedule of potential outcomes and assigned treatments for illustration of a downstream experiment
-#'
-#' @format A data.frame
-"table_6.4"
-
-#' Table 7.1 from FEDAI
-#'
-#' Hypothetical potential outcomes for eight subjects
-#'
-#' @format A data.frame
-"table_7.1"
-
-#' Table 7.2 from FEDAI
-#'
-#' Complete set of potential outcomes and covariates for eight subjects
-#'
-#' @format A data.frame
-"table_7.2"
-
 #' arceneaux_2005 from FEDAI
 #'
 #' Arceneaux, K. (2005). Using Cluster Randomized Field Experiments to Study Voting Behavior. The ANNALS of the American Academy of Political and Social Science, 601(1), 169-179
@@ -97,6 +6,12 @@
 #' @format A data.frame
 "arceneaux_2005"
 
+#' Box 3.7 from FEDAI
+#'
+#' This hypothetical dataset has two columns, a realized treatment (`Z`) and a revealed outcome (`Y`). The purpose of the dataset is to illustrate how traditional t-tests and randomization inference can yield different answers
+#'
+#' @format A data.frame
+"box_3_7"
 
 #' clingingsmith_etal_2009 from FEDAI
 #'
@@ -106,10 +21,148 @@
 #' The Quarterly Journal of Economics, Volume 124, Issue 3, August 2009, Pages 1133–1170,
 #' https://doi.org/10.1162/qjec.2009.124.3.1133
 #'
-#' These data describe 958 who were randomly selected to go on Hajj via lottery. The outcome Y_views describes these individuals' post-treatment views of others.
+#' These data describe 958 Pakistani Muslims who were randomly selected to go on Hajj via lottery. The outcome Y_views describes these individuals' post-treatment views of others.
 #'
 #' @format A data.frame
 "clingingsmith_etal_2009"
+
+#' rosen_2010 from FEDAI
+#'
+#' Jeff Rosen
+#' The Effects of Race and Grammar Quality on the Responsiveness of American State Legislators: A Field Experiment,
+#' Working paper, 2010.
+#'
+#' These data describe 400 observations of state legislators who were sent letters requesting information about immigration rules. The race of the sender was randomly manipulated through the name (Colin or Jose), as was the grammar of the letter (good or bad). The dataset records whether the request received a response and whether the legislator is themselves hispanic or not.
+#'
+#' @format A data.frame
+"rosen_2010"
+
+#' Table 2.1 from FEDAI
+#'
+#' This hypothetical dataset shows the treated (`Y_Z_1`) and untreated (`Y_Z_0`) potential outcomes for all seven villages, as well as a column for their difference, the village-level treatment effect `tau_i`
+#'
+#' @format A data.frame
+"table_2_1"
+
+#' Table 2.2 from FEDAI
+#'
+#' This hypothetical dataset shows a realized assignment (`Z`) and the corresponding revealed outcome (`Y`). Unobserved treated (`Y_Z_1`) and untreated (`Y_Z_0`) potential outcomes are marked as `NA` as is the village-level treatment effect `tau_i`.
+#'
+#' @format A data.frame
+"table_2_2"
+
+#' Table 3.3 from FEDAI
+#'
+#' This hypothetical dataset has the treated (`Y_Z_1`) and untreated (`Y_Z_0`) potential outcomes for 14 villages in two blocks.
+#'
+#' @format A data.frame
+"table_3_3"
+
+#' Table 3.6 from FEDAI
+#'
+#' This hypothetical dataset has the treated (`Y_Z_1`) and untreated (`Y_Z_0`) potential outcomes for 12 classrooms in 4 schools (high sampling variability)
+#'
+#' @format A data.frame
+"table_3_6"
+
+#' Table 3.7 from FEDAI
+#'
+#' This hypothetical dataset has the treated (`Y_Z_1`) and untreated (`Y_Z_0`) potential outcomes for 12 classrooms in 4 schools (low sampling variability)
+#'
+#' @format A data.frame
+"table_3_7"
+
+#' Table 4.1 from FEDAI
+#'
+#' Schedule of potential outcomes, pretest scores, and group assignments for simulated teacher incentives experiment
+#'
+#' @format A data.frame
+"table_4_1"
+
+#' Table 4.2 from FEDAI
+#'
+#' Block random assignment of teacher incentives experiment
+#'
+#' @format A data.frame
+"table_4_2"
+
+#' Table 5.1 from FEDAI
+#'
+#' Hypothetical schedule of potential outcomes assuming one-sided noncompliance
+#'
+#' @format A data.frame
+"table_5_1"
+
+#' Table 6.1 from FEDAI
+#'
+#' Hypothetical schedule of potential outcomes assuming two-sided noncompliance
+#'
+#' @format A data.frame
+"table_6_1"
+
+#' Table 6.4 from FEDAI
+#'
+#' Hypothetical schedule of potential outcomes and assigned treatments for illustration of a downstream experiment
+#'
+#' @format A data.frame
+"table_6_4"
+
+#' Table 7.1 from FEDAI
+#'
+#' Hypothetical potential outcomes for eight subjects.
+#'
+#' @format A data.frame
+"table_7_1"
+
+#' Table 7.2 from FEDAI
+#'
+#' Complete set of potential outcomes and covariates for eight subjects.
+#'
+#' @format A data.frame
+"table_7_2"
+
+#' Table 8.1 from FEDAI
+#'
+#' Hypothetical potential outcomes for three employees depending on who is honored with an employee of the month award.
+#'
+#' @format A data.frame
+"table_8_1"
+
+#' Table 8.2 from FEDAI
+#'
+#' Hypothetical potential outcomes for 5 villages depending on their treatment status and the treatment status of adjacent villages.
+#'
+#' @format A data.frame
+"table_8_2"
+
+#' Table 8.4 from FEDAI
+#'
+#' Hypothetical potential outcomes for 30 policing hotspots locations depending on their treatment status and the treatment status of adjacent hotspots.
+#'
+#' @format A data.frame
+"table_8_4"
+
+#' Distance matrix for Table 8.4 from FEDAI
+#'
+#' Distance matrix for the 30 hypothetical policing hotspots locations described in Table 8.4
+#'
+#' @format A data.frame
+"table_8_4_distmat"
+
+#' Table 8.7 from FEDAI
+#'
+#' Hypothetical potential outcomes for 8 media markets over three weeks, depending on current and previous period treatment status.
+#'
+#' @format A data.frame
+"table_8_7"
+
+#' Table 10.1 from FEDAI
+#'
+#' Hypothetical potential outcomes for six kinds of subjects
+#'
+#' @format A data.frame
+"table_10_1"
+
 
 
 
